8f77a14cbc573a61dc4519aeef64d729397cab83
maquinaadmin <maquinaadmin@MAQUINAVISUAL>
2017-02-17 20:05:21 +0000

    juubjjob job ojbouboub

---
+++ Practicas480/js/JavaScript.js
@@ -0,0 +1,183 @@
+﻿//1EventosYAccesoAControles
+function sumarNumeros() {
+    var caja1 = document.getElementById("num1");
+    var caja2 = document.getElementById("num2");
+    var num1 = parseInt(caja1.value);
+    var num2 = parseInt(caja2.value);
+    var resultado = document.getElementById("resultado");
+    var suma = num1 + num2;
+    resultado.innerText = suma;
+    alert("Numeros sumados");
+}
+
+//2Sentencias
+function mostrarEstacion() {
+    var caja = document.getElementById("caja");
+    var resultado = document.getElementById("resultado");
+    var num = parseInt(caja.value);
+    switch (num) {
+        case 1:
+            resultado.innerText = "INVIERNO";
+            break;
+        case 2:
+            resultado.innerText = "VERANO";
+            break;
+        case 3:
+            resultado.innerText = "PRIMAVERA";
+            break;
+        case 4:
+            resultado.innerText = "OTOÑO";
+            break;
+        default:
+            resultado.innerText = "Resultado incorrecto";
+    }
+}
+
+//3CalcularLetraDNi
+function calcularLetra()
+{
+    var resultado = document.getElementById("resultado");
+    var caja = document.getElementById("caja");
+    var dni = parseInt(caja.value);
+    var oper = (dni - (parseInt(dni / 23) * 23));
+    var letra;
+    alert(oper);
+    switch (oper) {
+        case 0:
+            letra = "T";
+            break;
+        case 1:
+            letra = "R";
+            break;
+        case 2:
+            letra = "W";
+            break;
+        case 3:
+            letra = "A";
+            break;
+        case 4:
+            letra = "G";
+            break;
+        case 5:
+            letra = "M";
+            break;
+        case 6:
+            letra = "Y";
+            break;
+        case 7:
+            letra = "F";
+            break;
+        case 8:
+            letra = "P";
+            break;
+        case 9:
+            letra = "D";
+            break;
+        case 10:
+            letra = "X";
+            break;
+        case 11:
+            letra = "B";
+            break;
+        case 12:
+            letra = "N";
+            break;
+        case 13:
+            letra = "J";
+            break;
+        case 14:
+            letra = "Z";
+            break;
+        case 15:
+            letra = "S";
+            break;
+        case 16:
+            letra = "Q";
+            break;
+        case 17:
+            letra = "V";
+            break;
+        case 18:
+            letra = "H";
+            break;
+        case 19:
+            letra = "L";
+            break;
+        case 20:
+            letra = "C";
+            break;
+        case 21:
+            letra = "K";
+            break;
+        case 22:
+            letra = "E";
+            break;
+        case 23:
+            letra = "T";
+            break;
+        default:
+            letra = "ERROR";
+    }
+    resultado.innerText = letra;
+}
+
+//4DiaNacimientoSemana
+function calcularDia() {
+    var cajadia = document.getElementById("cajadia");
+    var cajames = document.getElementById("cajames");
+    var cajaano = document.getElementById("cajaano");
+    var dia = parseInt(cajadia.value);
+    var mes = parseInt(cajames.value);
+    var ano = parseInt(cajaano.value);
+    if (mes == 1) {
+        mes = 13;
+        ano = ano - 1;
+    }
+    if (mes == 2) {
+        mes = 14;
+        ano = ano - 1;
+    }
+    alert(ano);
+    var oper = parseInt(((mes + 1) * 3) / 5);
+    var oper2 = parseInt(ano / 4);
+    var oper3 = parseInt(ano / 100);
+    var oper4 = parseInt(ano / 400);
+    var oper5 = parseInt(dia + (mes * 2) + ano + oper + oper2 - oper3 + oper4 + 2);
+    var oper6 = parseInt(oper5 / 7);
+    var oper7 = parseInt(oper5 - (oper6 * 7));
+    var resultado = document.getElementById("resultado");
+    resultado.innerText = oper7;
+}
+
+//ConjeturaCollatz
+function calcularCollatz() {
+    var caja = document.getElementById("caja");
+    var num = parseInt(caja.value);
+    var secuencia = "<ul>";
+    while (num != 1) {
+        secuencia += "<li>";
+        if (num % 2 == 0) {
+            num = num / 2;
+            
+        } else {
+            num = (num * 3) + 1;
+        }
+        secuencia += num  + "</li>";        
+    }
+    secuencia += "</ul>";
+    var resultado = document.getElementById("resultado");
+    resultado.innerHTML = secuencia;
+}
+//TablaMultiplicar
+function tablaMultiplicar() {
+    var caja = document.getElementById("caja");
+    var num = parseInt(caja.value);
+    var secuencia = "</ul>";
+    for (var i = 0; i <= 10; i++){
+        var oper = num * i;
+        secuencia += "<li>" + num + " x " + i + " = " + oper;
+    }
+    secuencia += "</ul>"
+    var resultadao = document.getElementById("resultado");
+    resultado.innerHTML = secuencia
+}
